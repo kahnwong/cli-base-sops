4a615df3370f5ba95f5b1f37e770e1838a921752
Karn Wong <karn@karnwong.me>
2026-01-30 23:27:17 +0700

    refactor: err as function return

--- config_sops.go
+++ config_sops.go
@@ -1,40 +1,44 @@
 package cli_base_sops
 
 import (
+	"fmt"
+
 	cliBase "github.com/kahnwong/cli-base"
 	"gopkg.in/yaml.v3"
 
 	"github.com/getsops/sops/v3/decrypt"
-	"github.com/rs/zerolog/log"
 )
 
-func decryptSops(path string, format string) []byte {
+func decryptSops(path string, format string) ([]byte, error) {
 	data, err := decrypt.File(path, format) // format: yaml, txt, etc. Refer to sops docs.
 	if err != nil {
-		log.Fatal().Msgf("Failed to decrypt sops config at: %s", path)
+		return nil, fmt.Errorf("failed to decrypt sops config at %s: %w", path, err)
 	}
 
-	return data
+	return data, nil
 }
 
-func ReadYamlSops[T any](path string) *T {
+func ReadYamlSops[T any](path string) (*T, error) {
 	// check if config exists
 	path, err := cliBase.CheckIfConfigExists(path)
 
 	if err != nil {
-		log.Fatal().Msgf("Config doesn't exist at: %s", path)
+		return nil, fmt.Errorf("config doesn't exist at %s: %w", path, err)
 	}
 
 	// decrypt sops
-	data := decryptSops(path, "yaml")
+	data, err := decryptSops(path, "yaml")
+	if err != nil {
+		return nil, err
+	}
 
 	// unmarshall
 	// ref: <https://stackoverflow.com/a/71955439>
 	config := new(T)
 	err = yaml.Unmarshal(data, &config)
 	if err != nil {
-		log.Fatal().Msgf("Error unmarshalling config: %s", path)
+		return nil, fmt.Errorf("error unmarshalling config at %s: %w", path, err)
 	}
 
-	return config
+	return config, nil
 }
--- go.mod
+++ go.mod
@@ -5,7 +5,6 @@ go 1.25.0
 require (
 	github.com/getsops/sops/v3 v3.11.0
 	github.com/kahnwong/cli-base v0.0.0-20250814120046-dde1e03ae9dd
-	github.com/rs/zerolog v1.34.0
 	gopkg.in/yaml.v3 v3.0.1
 )
 
@@ -94,6 +93,7 @@ require (
 	github.com/pkg/browser v0.0.0-20240102092130-5ac0b6a4141c // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/planetscale/vtprotobuf v0.6.1-0.20240319094008-0393e58bdf10 // indirect
+	github.com/rs/zerolog v1.34.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/ryanuber/go-glob v1.0.0 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
